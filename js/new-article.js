b956781cb8a93e5a2457529b10bfee3fe3d5d623
Yuri Faria <yuri.yurifaria@gmail.com>
2020-04-29 19:28:30 -0300

    js files

--- js/new-article.js
+++ js/new-article.js
@@ -3,6 +3,14 @@ const viewPreview = document.querySelector('#view-preview');
 const form = document.querySelector('form');
 const body = document.querySelector('body');
 
+function escapeRegExp(string) {
+  return string.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&');
+}
+
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+
 function openPreview() {
   const modal = document.querySelector('.modal-background');
   const article = document.querySelector('article');
@@ -12,6 +20,11 @@ function openPreview() {
 
   article.innerHTML = `
     <h2>${form.title.value}</h2>
+    <div class="tag-list">
+      ${ replaceAll(form.tags.value, ', ', ',').split(',').map(tag => `<div class="tag">${ tag.toUpperCase() }</div>`) }
+      <div class="tag">BY: YURI FARIA</div>
+      <div class="tag">27/JAN/2020</div>
+    </div>
     ${form.text.value}
   `;
 }
